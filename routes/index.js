a9cfeed823a5858106ee2978f90d662aed2b5b5b
Nicholas King <enotirab@mail.fresnostate.edu>
2022-01-22 16:17:15 -0800

    Student crap done.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const courseController = require('../controllers/courseController.js');
+const studentController = require('../controllers/studentsController.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,10 +12,18 @@ router.get('/courses', courseController.viewAll);
 router.get('/courses/profile/:id', courseController.viewProfile);
 router.get('/courses/add', courseController.renderAddForm);
 router.post('/courses/add', courseController.addCourse);
+router.get('/courses/edit/:id', courseController.renderEditForm);
+router.post('/courses/edit/:id', courseController.updateCourse);
 router.get('/courses/delete/:id', courseController.deleteCourse);
 
 
-router.get('/courses/edit/:id', courseController.renderEditForm);
-router.post('/courses/edit/:id', courseController.updateCourse);
+router.get('/students', studentController.viewAll);
+router.get('/students/profile/:id', studentController.viewProfile);
+router.get('/students/add', studentController.renderAddForm);
+router.post('/students/add', studentController.addStudent);
+router.get('/students/edit/:id', studentController.renderEditForm);
+router.post('/students/edit/:id', studentController.updateStudent);
+router.get('/students/delete/:id', studentController.deleteStudent);
+
 
 module.exports = router;
